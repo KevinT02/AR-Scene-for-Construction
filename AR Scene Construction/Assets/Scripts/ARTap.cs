53b8ffa8cf45093ae26762559b47397671217553
Kevin T <tu02.kevin@gmail.com>
2020-11-14 15:39:48 -0500

    Added rotation UI

--- AR Scene Construction/Assets/Scripts/ARTap.cs
+++ AR Scene Construction/Assets/Scripts/ARTap.cs
@@ -2,73 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-//using UnityEngine.Experimental.XR;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 using System;
 
 
 
 public class ARTap : MonoBehaviour
 {
-    public GameObject objectToPlace;
-    public GameObject placementIndicator;
 
     //private ARSessionOrigin arOrigin;
-    private ARRaycastManager aRRaycastManager;
+    private ARRaycastManager raycastManager;
     private Pose placementPose;
-    private bool placementPoseIsValid = false;
-    
-    
-    
-    void Start()
+    private bool placementPoseIsValid;
+    private bool isObjectPlaced;
+
+
+    public GameObject positionIndicator;
+    public GameObject prefabToPlace;
+    public Camera ARCamera;
+    public Canvas canvas;
+
+  
+    private void Awake()
     {
-        //arOrigin = FindObjectOfType<ARSessionOrigin>();
-        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+        raycastManager = GetComponent<ARRaycastManager>();
     }
+
     void Update()
     {
         UpdatePlacementPose();
-        UpdatePlacementIndicator();
+
         if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             PlaceObject();
         }
+
     }
-   
-    
-    
-    private void PlaceObject()
-    {
-        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
-    }
-    private void UpdatePlacementIndicator()
-    {
-        if (placementPoseIsValid)
-        {
-            placementIndicator.SetActive(true);
-            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
-        }
-        else
-        {
-            placementIndicator.SetActive(false);
-        }
-    }
-    
-    
-    
+
+
     private void UpdatePlacementPose()
     {
-        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var screenCenter = ARCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         var hits = new List<ARRaycastHit>();
-        //arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
-        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        raycastManager.Raycast(screenCenter, hits, TrackableType.All);
+
         placementPoseIsValid = hits.Count > 0;
+
         if (placementPoseIsValid)
         {
             placementPose = hits[0].pose;
-            var cameraForward = Camera.current.transform.forward;
+            var cameraForward = ARCamera.transform.forward;
             var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
             placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            positionIndicator.SetActive(true);
+            positionIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         }
+        else
+        {
+            positionIndicator.SetActive(false);
+        }
+    }
+
+    private void PlaceObject()
+    {
+        var spawnedObject = Instantiate(prefabToPlace, placementPose.position, placementPose.rotation);
+        RotateManager.GetInstance().SetBuilding(spawnedObject);
+
+        isObjectPlaced = true;
+        positionIndicator.SetActive(false);
+        canvas.gameObject.SetActive(true);
     }
 }
